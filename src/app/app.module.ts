a5f54490e61e86a1755683ca408827dbc59fcadc
Nabeel Hassan <nabeelhassan_17@hotmail.com>
2019-12-29 23:02:56 +0500

    learnt graphql from 'https://github.com/arjunyel/angular-spacex-graphql-codegen'

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material';
+
+import { AppComponent } from './app.component';
+import { LaunchListComponent } from './launch-list/launch-list.component';
+import { LaunchDetailsComponent } from './launch-details/launch-details.component';
+import { AppRoutingModule } from './app-routing.module';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RelativeTimePipe } from './relative-time/relative-time.pipe';
+
+@NgModule({
+  declarations: [AppComponent, LaunchListComponent, LaunchDetailsComponent, RelativeTimePipe],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    GraphQLModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/launch-list/launch-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+import { PastLaunchesListGQL } from '../services/spacexGraphql.service';
+
+@Component({
+  selector: 'app-launch-list',
+  templateUrl: './launch-list.component.html',
+  styleUrls: ['./launch-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LaunchListComponent {
+  constructor(private readonly pastLaunchesService: PastLaunchesListGQL) {}
+
+  pastLaunches$ = this.pastLaunchesService
+    .fetch({ limit: 10 })
+    .pipe(map(res => res.data.launchesPast));
+}
